6e56c024fee883600eaa1f78d6fc073a92b760b5
Aliya2003-max <palagirialiya2003@gmail.com>
2025-06-28 10:38:02 +0530

    Add files via upload

---
+++ t3/my_tennis_club/members/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+class Member(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname =  models.CharField(max_length=30)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+
+    def __str__(self):
+        return self.firstname
+
+    
+
+
+
